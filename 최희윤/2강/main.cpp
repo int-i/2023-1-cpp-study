7e583eb04435fa965044f59e06e666bb5ca93567
최희윤 <chy7329508@naver.com>
2023-03-18 22:42:04 +0900

    [Homework] 최희윤 2강

--- 최희윤/2강/main.cpp
+++ 최희윤/2강/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-	cout << "Hello inti!" << endl;
+	int N = 0;
+	int X = 0;
+
+	cin >> N >> X;
+
+	int A = 0;
+
+	for (int i = 0; i < N; i++) {
+		cin >> A;
+		if (A < X) // A가 X보다 작다면,
+			cout << A << " ";
+	}
+
 	return 0;
 }
