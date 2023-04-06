b4861d334dbf122a3062104cba9c3c31875d1931
지나이 지나이 <lsj77205619@gmail.com>
2023-04-06 09:04:58 +0900

    [Homework] 이승준 4강 과제

---
+++ 이승준/4강/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double sum = 0;
+    int hscore = 0;
+    int n = 0;
+    cout << "과목의 개수를 입력하시오." << endl;
+    cin >> n;
+    float score[1000];
+    cout << "각 과목의 점수를 입력하시오." << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> score[i]; //점수 입력 받기
+    }
+    for (int i = 0; i < n; i++) {
+        if (score[i] >= hscore) {
+            hscore = score[i];
+            //최고점 찾기
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        sum = 100 * score[i] / hscore + sum; //각 점수 고치고 더하기
+    }
+    cout << sum / n;
+
+
+}
